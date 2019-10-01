6db897c8a73a0f74a335a9d454760231eac8b390
Maxim Baykevich <max.baikevich@gmail.com>
2019-10-02 00:54:38 +0300

    angular-lesson1

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello {{name}}</h1>`,
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent { name = 'Angular'; }
+
+
